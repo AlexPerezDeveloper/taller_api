e537eec000f8c75f325aa51c7a1f2a893a6d53e8
AlexPerezDeveloper <alexperez.developer@gmail.com>
2019-08-21 02:26:50 +0200

    added changes into db connect

--- config/database.js
+++ config/database.js
@@ -73,11 +73,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'manny.db.elephantsql.com'),
+      host: Env.get('DB_HOST', 'ec2-46-137-113-157.eu-west-1.compute.amazonaws.com'),
       port: Env.get('DB_PORT', '5432'),
-      user: Env.get('DB_USER', 'mgyvbjgj'),
-      password: Env.get('DB_PASSWORD', 'OgfifCX-EwRpFIV9NmOUHCDnE7z9PSx4'),
-      database: Env.get('DB_DATABASE', 'protaller')
+      user: Env.get('DB_USER', 'dc3hc2td5sugg4'),
+      password: Env.get('DB_PASSWORD', 'df7722bdc4002eb86451a49b7c9a0b584e83f66ac307fec5474afcd754caa213'),
+      database: Env.get('DB_DATABASE', 'dc3hc2td5sugg4')
     },
     debug: Env.get('DB_DEBUG', false)
   }
